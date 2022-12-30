4be248befebbc553d3c47bc447b3b0b53a67a828
tarikwaleed <takizee53@gmail.com>
2022-12-30 10:32:29 +0200

    :hammer:

--- create_database.sh
+++ create_database.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-source ./helper_functions.sh
-# cd ~/bash_db_engine
 echo -e "Database Name: "
 read database_name
 if [ -d ~/bash_db_engine/$database_name ]; then
     echo -e "Database with name $database_name already exists"
 else
-    create_database $database_name
+    mkdir -p ~/bash_db_engine/${database_name}
+    echo -e "Your Database is created Successfully \n"
+    # create_database $database_name
     echo -e "Would you like to connect to this DB (y/n) ?"
     read confirmation
     if [ $confirmation == "y" ]; then
-        cd ~/bash_db_engine/$database_name
+        cd $HOME/bash_db_engine/$database_name
+        pwd
         echo "cded into directory"
     fi
 fi
--- helper_functions.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-create_database() {
-    database_name=$1
-    mkdir -p ~/bash_db_engine/${database_name}
-    echo -e "Your Database is created Successfully \n"
-}
