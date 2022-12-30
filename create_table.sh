7fdc5c368b5179854e8f964dc3cac67094587907
tarikwaleed <takizee53@gmail.com>
2022-12-30 14:13:53 +0200

    :sparkles: done with validating table name

--- create_table.sh
+++ create_table.sh
@@ -1,18 +1,30 @@
 function validate_user_input() {
     #? the input doesn't include spaces because we are taking the first argumet
     #? passed to the function
-    user_input=$1
+    user_input=$@
     first_character_in_input=${user_input:0:1}
-    input_is_valid=1
+    if [[ $# -gt 1 ]]; then
+        echo "table name cann't have spaces"
+        return 0
+    fi
+
     if [[ $# -eq 0 ]]; then
         echo "No argument supplied"
-        input_is_valid=0
+        return 0
     fi
     if [[ $first_character_in_input =~ [0-9] ]]; then
         echo "table name cann't start with number"
-        input_is_valid=0
+        return 0
     fi
 
-    echo $input_is_valid
+    return 1
 }
-validate_user_input $1
+
+while :
+do
+    echo "Enter Table name: "
+    read table_name
+    validate_user_input $table_name
+    returnvalue=$?
+    echo $returnvalue
+done
