800a34b7ef936545f8741421efc8cbd680d7e77f
tarikwaleed <takizee53@gmail.com>
2022-12-31 20:48:02 +0200

    changes

--- create_table.sh
+++ create_table.sh
@@ -31,7 +31,7 @@ function touch_file() {
     fi
 
 }
-function validate_data_type_input() {
+function validate_column_type() {
     user_input=$@
     first_character_in_input=${user_input:0:1}
     if [[ $# -gt 1 ]]; then
@@ -43,49 +43,89 @@ function validate_data_type_input() {
         echo "No argument supplied"
         return 0
     fi
-    if [[ $first_character_in_input =~ [sSiI] ]]; then
+    if [ $first_character_in_input == 's' ] || [ $first_character_in_input == 'S' ] || [ $first_character_in_input == 'i' ] || [ $first_character_in_input == 'I' ]; then
         return 1
     fi
 
-
 }
-function main_logic() {
-    echo "Enter Table name: "
-    read table_name
-    validate_user_input $table_name
-    table_name_is_valid=$?
+function validate_number_of_columns() {
+    user_input=$1
+    regular_expression='^[0-9]+$'
+    if ! [[ $user_input =~ $regular_expression ]]; then
+        echo "error: Not a number"
+        return 0
+    else
+        return 1
+    fi
+}
 
+function main_logic() {
+    table_name_is_valid=0
+    while [[ table_name_is_valid -eq 0 ]]; do
+        echo "Enter Table name: "
+        read table_name
+        validate_user_input $table_name
+        table_name_is_valid=$?
+        if [[ table_name_is_valid -eq 0 ]]; then
+            echo "Invalid input, Try Again"
+        fi
+    done
     if [[ table_name_is_valid -eq 1 ]]; then
         touch_file $table_name
         file_touched_successfully=$?
         if [[ file_touched_successfully -eq 1 ]]; then
             echo "file touched successfully"
-            echo "Enter number of columns in the table:"
-            read number_of_columns
-            typeset -i count=$number_of_columns
-            while [[ $count -lt $number_of_columns ]]; do
-                #todo: do the following for each column
-                #todo: take name of the column from user
-                echo "Enter name of column"
-                read name_of_column
-                #todo: validate that name
-                validate_user_input $name_of_column
-                column_name_is_valid=$?
-                #todo: valid?
-                if [[ column_name_is_valid -eq 1 ]]; then
-                    #todo: column type
-                    echo "Chose column type [(i/s) chose i for integer s for string "
-                    read column_type
-                    #todo: validate_data_type_input
-                    validate_data_type_input $column_type
-                    column_type_is_valid=$?
-                    #! should be either s or i
-
-                #todo: is_primary_key
-
+            number_of_columns_is_valid=0
+            while [[ number_of_columns_is_valid -eq 0 ]]; do
+                echo "Enter number of columns in the table:"
+                read number_of_columns
+                #TODO: create this function
+                validate_number_of_columns $number_of_columns
+                number_of_columns_is_valid=$?
+                if [[ number_of_columns_is_valid -eq 0 ]]; then
+                    echo "Invalid input, Try Again"
                 fi
 
-                count=$(($count - 1))
+            done
+
+            typeset -i count=0
+            declare -i primary_key_determined=0
+            while [[ $count -lt $number_of_columns ]]; do
+                #? column name
+                column_name_is_valid=0
+                while [[ column_name_is_valid -eq 0 ]]; do
+                    echo "Enter name of column ${count} "
+                    read name_of_column
+                    validate_user_input $name_of_column
+                    column_name_is_valid=$?
+                    if [[ column_name_is_valid -eq 0 ]]; then
+                        echo "Invalid input, Try Again"
+                    fi
+                done
+                #? column type
+                column_type_is_valid=0
+                while [[ column_type_is_valid -eq 0 ]]; do
+                    echo "Chose column type [(i/s) Enter i for integer s for string "
+                    read column_type
+                    validate_column_type $column_type
+                    column_type_is_valid=$?
+                    if [[ column_type_is_valid -eq 0 ]]; then
+                        echo "Invalid input, Try Again"
+                    fi
+                done
+                #? is_primary_key
+                column_is_primary_key=0
+                if [[ primary_key_determined -eq 0 ]]; then
+                    echo "Is this column  a primary key: (y/n)"
+                    read is_primary_key_choice
+                    if [[ is_primary_key_choice == 'y' || is_primary_key_choice == 'Y' ]]; then
+                        primary_key_determined=1
+                        column_is_primary_key=1
+                    fi
+                    echo $name_of_column:$column_type:$column_is_primary_key
+                fi
+
+                count=$(($count + 1))
             done
 
         else
@@ -93,7 +133,4 @@ function main_logic() {
         fi
     fi
 }
-
-validate_data_type_input s
-column_type_is_valid=$?
-echo $column_type_is_valid
+main_logic
